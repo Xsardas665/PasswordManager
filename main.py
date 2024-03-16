6ce054ee8272eb490fc796395830d272f861d81e
Maciej Zygadło <Xsardas665@gmail.com>
2024-03-16 01:40:34 +0100

    Adding hashlib library, some missing comments, master password hashing function and TODO Task.

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+# hashlib is a library that provides secure hash functions,
+# including SHA-256, for creating hash values from data,
+# often used for password hashing and data integrity verification.
+import hashlib
 from datetime import datetime
 from datetime import date
 
 
+# Function for creating SHA256 hash of master password
+def hash_master_password(password):
+    sha256 = hashlib.sha256()
+    sha256.update(password.encode())
+    return sha256.hexdigest()
+
+
+# Custom Function for logging into Console.
+# TODO: Add logging into logfile
 def print_log(log_type, text):
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
